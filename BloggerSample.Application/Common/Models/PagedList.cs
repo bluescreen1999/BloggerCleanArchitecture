4cb1940e439ebf8971321bd1a825bd7fb80c30bc
mohammad <mohammad.zr1378@gmail.com>
2023-12-18 16:04:47 +0330

    EntityFrameworkCore removed from Application layer

--- BloggerSample.Application/Common/Models/PagedList.cs
+++ BloggerSample.Application/Common/Models/PagedList.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace BloggerSample.Application.Common.Models
+﻿namespace BloggerSample.Application.Common.Models
 {
     public class PagingParams
     {
